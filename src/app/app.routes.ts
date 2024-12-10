09d92ffee0e27fad525531d2a31abc19e0ec2066
jcapitanmoreno <jcapitanmoreno@gmail.com>
2024-12-10 10:42:12 +0100

    crud hecho

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,8 @@
 import { Routes } from '@angular/router';
+import { HomeComponent } from './pages/home/home.component';
+import { LolCharactersComponent } from './pages/lol-characters/lol-characters.component';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+    { path: '', component: HomeComponent },
+    { path: 'lol-characters', component: LolCharactersComponent }
+];
---
+++ src/app/pages/lol-characters/lol-characters.component.ts
@@ -0,0 +1,12 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-lol-characters-list',
+  standalone: true,
+  imports: [],
+  templateUrl: './lol-characters.component.html',
+  styleUrl: './lol-characters.component.css'
+})
+export class LolCharactersComponent {
+
+}
